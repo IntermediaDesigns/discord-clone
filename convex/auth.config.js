c159a798bac9866587e565efbeb8931781d5e824
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-12-26 11:44:28 -0500

    Remove unused configuration files and components; clean up project structure

--- convex/auth.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://natural-octopus-14.clerk.accounts.dev",
-      applicationID: "clerk",
-      tokenVerification: "automatic"
-    }
-  ]
-};
